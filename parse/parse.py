1a7f05154e3f025666f20785b8cf5adfa0e69af8
Sean Rose <sean.andrew.rose@gmail.com>
2013-01-16 11:12:06 -0800

    reorg of ParseClient class

--- parse/parse.py
+++ parse/parse.py
@@ -24,6 +24,26 @@ def _set_parse_credential_from_env():
     return application_id, rest_api_key
 
 
+def _generate_parse_url(self, object_class, object_id=None,):
+    """
+    Builds the complete Parse URL for making requests
+    """
+
+    if object_class == 'users' or object_class == 'login':
+        resource_alias = ''
+    else:
+        resource_alias = '/classes'
+
+    if object_id:
+        url = '%s%s/%s/%s' % (self.url, resource_alias,
+                              object_class, object_id)
+    else:
+        url = '%s%s/%s' % (self.url, resource_alias,
+                           object_class)
+
+    return url
+
+
 class ParseClient(object):
     """A wrapper for interacting with the Parse API
 
@@ -52,8 +72,18 @@ class ParseClient(object):
 
     @raises_parse_error
     def create_object(self, object_class, object_attributes):
+        """
+        Creates an object by making a POST request its class
+        https://parse.com/docs/rest#objects-creating
+
+        Args:
+            object_class: String representing the class name
+            object_attributes: dict containing the object's attributes
+        """
+
         url = self._generate_parse_url(object_class)
         headers = {'content-type': 'application/json'}
+
         response = self.rest_client.post(url, headers=headers,
                                          data=json.dumps(object_attributes))
         return response.json()
@@ -61,36 +91,63 @@ class ParseClient(object):
     @raises_parse_error
     def get_object(self, object_class, object_id, include=None,
                    login_credentials=None):
+        """
+        Retrieves an object based on its ID.
+
+        Args:
+            object_class: String representing the class name
+            object_id: String ID of this object
+            inlcude: attribute names of sub-objects to include instead of pointers
+        """
+
         url = self._generate_parse_url(object_class, object_id)
         params = {}
+
         if login_credentials is not None:
             params = login_credentials
         if include is not None:
             params['include'] = ','.join(include)
+
         response = self.rest_client.get(url, params=params)
         return response.json()
 
     @raises_parse_error
     def delete_object(self, object_class, object_id, auth=None):
+        """
+        Attempts to delete an object.
+
+        Returns:
+            True if the delete was successful, False if it was not
+        """
         url = self._generate_parse_url(object_class, object_id)
-        headers = {}
+
         if auth is not None:
             headers = {'X-Parse-Session-Token': auth}
+        else:
+            headers = {}
+
         response = self.rest_client.delete(url, headers=headers)
         deletion_result = (response.status_code == requests.codes.ok)
         return deletion_result
 
     @raises_parse_error
     def update_object(self, object_class, object_id, object_attributes):
+        """
+        Updates a specific set of attributes for a specific object
+        """
+
         url = self._generate_parse_url(object_class, object_id)
         headers = {'content-type': 'application/json'}
+
         response = self.rest_client.put(url,
                                         headers=headers,
                                         data=json.dumps(object_attributes))
         return response.json()
 
     @raises_parse_error
-    def query_object_class(self, object_class, constraints,
+    def query_object_class(self,
+                           object_class,
+                           constraints,
                            order='',
                            include=None,
                            count=False,
@@ -108,7 +165,7 @@ class ParseClient(object):
             params['limit'] = 0
 
         response = self.rest_client.get(url, params=params)
-
+        # TODOD: make this suck less
         if count:
             return response.json()['count']
         if response.json()['results']:
@@ -118,16 +175,3 @@ class ParseClient(object):
                 return response.json()['results']
         else:
             return None
-
-    def _generate_parse_url(self, object_class, object_id=None,):
-        if object_class is 'users' or object_class is 'login':
-            resource_alias = ''
-        else:
-            resource_alias = '/classes'
-        if object_id:
-            url = '%s%s/%s/%s' % (self.url, resource_alias,
-                                  object_class, object_id)
-        else:
-            url = '%s%s/%s' % (self.url, resource_alias,
-                               object_class)
-        return url
