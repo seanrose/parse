b7e3fe27b2e09980ce23a33408b9ede5e99ba152
Sean Rose <sean.andrew.rose@gmail.com>
2013-02-01 18:32:04 -0800

    first tests!

--- parse_tests.py
+++ parse_tests.py
@@ -1,3 +1,38 @@
+import unittest
+import os
 from parse import ParseClient
 
-p = ParseClient()
+
+class ParseTests(unittest.TestCase):
+
+    def testCanInitParseClientFromEnvVariables(self):
+
+        os.environ['PARSE_APPLICATION_ID'] = 'foo'
+        os.environ['PARSE_REST_API_KEY'] = 'bar'
+
+        p = ParseClient()
+
+        self.failUnless(
+            p.rest_client.headers['X-Parse-Application-Id'] == 'foo'
+        )
+        self.failUnless(
+            p.rest_client.headers['X-Parse-REST-API-Key'] == 'bar'
+        )
+
+    def testCanInitParseClientWithArgs(self):
+
+        p = ParseClient('foo', 'bar')
+
+        self.failUnless(
+            p.rest_client.headers['X-Parse-Application-Id'] == 'foo'
+        )
+        self.failUnless(
+            p.rest_client.headers['X-Parse-REST-API-Key'] == 'bar'
+        )
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
