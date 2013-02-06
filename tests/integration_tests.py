8132c8e70b3c82925cb1083a74070bab7ee41136
Sean Rose <sean.andrew.rose@gmail.com>
2013-02-05 18:52:33 -0800

    adding test schema for integration testing

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,9 +1,8 @@
 import os
 from ..parse import parse
 from nose import with_setup
-from nose.tools import ok_, timed
-
-TEST_CLASS = 'TestObjects'
+from nose.tools import ok_
+from test_object_schema import TEST_CLASS_NAME
 
 
 def dummy_env_setup():
@@ -13,52 +12,53 @@ def dummy_env_setup():
 
 # Create an object
 @with_setup(dummy_env_setup)
-@timed(1)
+# @timed(1)
 def test_create_object():
 
     p = parse.ParseClient()
 
-    obj = p.create_object(TEST_CLASS)
+    obj = p.create_object(TEST_CLASS_NAME)
 
     ok_(obj, 'Object was not successfully created')
 
 
 # Get an object
 @with_setup(dummy_env_setup)
-@timed(1)
+# @timed(1)
 def test_get_object():
 
     p = parse.ParseClient()
 
-    obj = p.query_object_class(TEST_CLASS, {}, first_result_only=True)
+    obj = p.query_object_class(TEST_CLASS_NAME, {}, first_result_only=True)
 
-    p.get_object(TEST_CLASS, obj['objectId'])
+    p.get_object(TEST_CLASS_NAME, obj['objectId'])
 
     ok_(obj, 'Object was not successfully gotten')
 
 
 # Update the object
 @with_setup(dummy_env_setup)
-@timed(1)
+# @timed(1)
 def test_update_object():
 
     p = parse.ParseClient()
 
-    obj = p.query_object_class(TEST_CLASS, {}, first_result_only=True)
+    obj = p.query_object_class(TEST_CLASS_NAME, {}, first_result_only=True)
 
-    updated_at = p.update_object(TEST_CLASS, obj['objectId'], {'foo': 'bar'})
+    updated_at = p.update_object(TEST_CLASS_NAME, obj['objectId'],
+                                 {'foo': 'bar'})
 
     ok_(updated_at, 'Object was not successfully updated')
 
 
 # Delete the object
 @with_setup(dummy_env_setup)
-@timed(1)
+# @timed(1)
 def test_delete_object():
 
     p = parse.ParseClient()
 
-    obj = p.query_object_class(TEST_CLASS, {}, first_result_only=True)
+    obj = p.query_object_class(TEST_CLASS_NAME, {}, first_result_only=True)
 
-    ok_(p.delete_object(TEST_CLASS, obj['objectId']),
+    ok_(p.delete_object(TEST_CLASS_NAME, obj['objectId']),
         'Object was not successfully deleted')
---
+++ tests/test_object_schema.py
@@ -0,0 +1,7 @@
+TEST_CLASS_NAME = 'TestObjects'
+
+TEST_CLASS_ATTRIBUTES = {
+    'name': 'Jon Snow',
+    'location': 'The Wall',
+    'age': 17
+}
